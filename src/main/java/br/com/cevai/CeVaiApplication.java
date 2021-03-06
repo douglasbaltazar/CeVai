e62433e8fb67e7b633129b45e40f0fa8114bd23a
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-03-06 19:49:14 -0300

    Commit inicial do BackEnd

---
+++ src/main/java/br/com/cevai/CeVaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cevai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeVaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeVaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cevai/CeVaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cevai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeVaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
